00e7fe6cdf89fde4cf7079bb47b8cc10a4524071
Kaiwen <huangkaiw3n@gmail.com>
2017-02-07 16:23:41 +0800

    Use firebase admin with auth for writing to firebase

--- api/firebaseDatabase.js
+++ api/firebaseDatabase.js
@@ -1,24 +1,22 @@
-var Firebase = require('firebase');
+var admin = require("firebase-admin");
+var serviceAccount = require("../parkpark-334b8-firebase-adminsdk-yple2-397f952766.json");
 
 let HAS_INITIALIZED = false
 
 const initFirebase = () => {
   if (!HAS_INITIALIZED) {
     var config = {
-      apiKey: "AIzaSyBzClvv2oiYFWRv1AbpwXz_KK84WiOLA-s",
-      authDomain: "parkpark-334b8.firebaseapp.com",
-      databaseURL: "https://parkpark-334b8.firebaseio.com",
-      storageBucket: "parkpark-334b8.appspot.com",
-      messagingSenderId: "141561941074"
+      credential: admin.credential.cert(serviceAccount),
+      databaseURL: "https://parkpark-334b8.firebaseio.com"
     };
 
-    Firebase.database.enableLogging(true)
-    Firebase.initializeApp(config)
+    admin.database.enableLogging(true)
+    admin.initializeApp(config)
     HAS_INITIALIZED = true
   }
 }
 
 exports.getDatabase = () => {
   initFirebase()
-  return Firebase.database()
+  return admin.database()
 }
